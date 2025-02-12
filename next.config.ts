1cbe5aab9a9e232c380d4f14cf286a879fc6eaa4
KennyNova <navidmadani4598@gmail.com>
2025-02-12 10:43:24 -0500

    nextjs config fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: '/', // This is the most important part!
+  // ... other configuration options (if any)
 };
 
 export default nextConfig;
